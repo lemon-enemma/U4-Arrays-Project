6f31a0f5ec5ffae58fd5d7dd4f98e84b4f554cee
EmmaLeung <emmal219@nycstudents.net>
2026-02-03 14:49:27 -0500

    2/3

--- src/Deck.java
+++ src/Deck.java
@@ -1,12 +1,133 @@
 public class Deck {
     private String[] cards;
     private String type;
+    private int[] numRepeats;
 
-    public Deck(String[] c){
+    public Deck(String[] c) {
         cards = c;
+        numRepeats = new int[5];
     }
 
-    public void findType(){
+    public void findType() {
+        for (int i = 0; i < cards.length; i++) {
+            for (int j = 0; j < cards.length; j++) {
+                int numMatches = 0;
+                if (cards[i].equals(cards[j]) && i != j) {
+                    numMatches++;
+                }
+                numRepeats[i] = numMatches;
+            }
+        }
+        if (fiveOfKind()){
+            type = "Five of a kind";
+        } else if (fourofKind()) {
+            type = "Four of a kind";
+        } else if (fullHouse()) {
+            type = "Full house";
+        } else if (threeofKind()) {
+            type = "Three of a kind";
+        } else if (twoPair()) {
+            type = "Two pair";
+        } else if (onePair()) {
+            type = "One pair";
+        }
+        else {
+            type = "High card";
+        }
+        System.out.println(type);
+    }
 
+    public Boolean fiveOfKind() {
+        Boolean b = false;
+        for (int repeat : numRepeats) {
+            if (repeat == 4) {
+                b = true;
+            }
+        }
+        return b;
+    }
+
+    public Boolean fourofKind() {
+        Boolean b = false;
+        for (int repeat : numRepeats) {
+            if (repeat == 3) {
+                b = true;
+            }
+        }
+        return b;
+    }
+
+    public Boolean fullHouse() {
+        Boolean b = false;
+        Boolean c = false;
+        for (int i = 0; i < numRepeats.length; i++) {
+            if (numRepeats[i] == 2) {
+                b = true;
+                for (int j = 0; j < numRepeats.length; j++) {
+                    if (numRepeats[j] == 1 && i != j) {
+                        c = true;
+                    }
+                }
+            }
+        }
+        return b && c;
+    }
+
+    public Boolean threeofKind() {
+        Boolean b = false;
+        Boolean c = true;
+        for (int i = 0; i < numRepeats.length; i++) {
+            if (numRepeats[i] == 2) {
+                b = true;
+                for (int j = 0; j < numRepeats.length; j++) {
+                    if (numRepeats[j] == 1 && i != j) {
+                        c = false;
+                    }
+                }
+            }
+        }
+        if (b && !c) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public Boolean twoPair() {
+        Boolean b = false;
+        Boolean c = false;
+        for (int i = 0; i < numRepeats.length; i++) {
+            if (numRepeats[i] == 1) {
+                b = true;
+                for (int j = 0; j < numRepeats.length; j++) {
+                    if (numRepeats[j] == 1 && i != j) {
+                        c = true;
+                    }
+                }
+            }
+        }
+        return b && c;
+    }
+
+    public Boolean onePair() {
+        Boolean b = false;
+        Boolean c = true;
+        for (int i = 0; i < numRepeats.length; i++) {
+            if (numRepeats[i] == 1) {
+                b = true;
+                for (int j = 0; j < numRepeats.length; j++) {
+                    if (numRepeats[j] == 1 && i != j) {
+                        c = false;
+                    }
+                }
+            }
+        }
+        if (b && !c) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -13,63 +13,9 @@ public class Main {
                 String line = s.nextLine();
                 fileData += line + "\n";
             }
-        }
-        catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e) {
             System.out.println("File not found");
         }
-        int numFiveKind = 0;
-        int numFullHouse = 0;
-        int numFourKind = 0;
-        int numThreeKind = 0;
-        int numTwoPair = 0;
-        int numOnePair = 0;
-        int numHighCard = 0;
-        String[] lines = fileData.split("\n");
-        for (String line : lines) {
-            int maxMatchingCards = 0;
-            String[] labels = line.split(" ");
-            for (int i = 0; i < labels.length; i++){
-                int numMatches = 0;
-                String currentLabel = labels[i];
-                for (int j = 0; j < labels.length; j++){
-                    if (j != i){
-                        if (currentLabel.equals(labels[j])){
-                            numMatches ++;
-                        }
-                    }
-                }
-                if (numMatches > maxMatchingCards){
-                    maxMatchingCards = numMatches;
-                }
-            }
-            if (maxMatchingCards == 4){
-                numFiveKind++;
-            } else if (maxMatchingCards == 3) {
-                numFourKind++;
-            } else if (maxMatchingCards == 2) {
-                int minMatch = 0;
-                for (int i = 0; i < labels.length; i++){
-                    int numMatch = 0;
-                    String currentLabel = labels[i];
-                    for (int j = 0; j < labels.length; j++){
-                        if (j != i){
-                            if (currentLabel.equals(labels[j])){
-                                numMatch++;
-                            }
-                        }
-
-                    }
-                    if (numMatch < minMatch){
-                        minMatch = numMatch;
-                    }
-            }
-                if (minMatch == 1){
-                    numFullHouse++;
-                } else {
-                    numThreeKind++;
-                }
-
-                }
-            }
+        System.out.println(fileData);
     }
 }
